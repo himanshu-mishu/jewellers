4eacbd2d57a6bba91d10366df3ff28f8bebd20b6
himanshu-mishu <himanshu.agrl111@gmail.com>
2025-11-05 15:15:04 +0530

    configured tailwind in project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <h1 class="text-3xl font-bold underline text-pink-500">
+  Tailwind Works! 💎
+</h1>
     </div>
   )
 }
