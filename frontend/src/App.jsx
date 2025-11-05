5bc907d1c9403170654afee1df577535fc8e97a9
himanshu-mishu <himanshu.agrl111@gmail.com>
2025-11-05 16:22:21 +0530

    addedpages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline text-pink-500">
-  Tailwind Works! 💎
-</h1>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] xl:px-[20vw] 2xl:px-[25vw] py-4'>
+      <Routes>
+        <Route path='/' element= {<Home/>} />
+      </Routes>
     </div>
   )
 }
