0731834f1ec76836ee596ea89add31cc91f5723d
himanshu-mishu <himanshu.agrl111@gmail.com>
2025-11-10 00:30:34 +0530

    added profile section with dropdown

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,26 +7,39 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       <img src={assets.logo} alt="logo" className='w-36'/>
-<ul className='sm:flex gap-5 text-sm text-gray-700'>
+<ul className='sm:flex gap-5 text-sm text-gray-700 '>
         <NavLink to='/' className='flex flex-col items-center gap-1'>
             <p>Home</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
            <NavLink to='/collection' className='flex flex-col items-center gap-1'>
             <p>Collection</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
            <NavLink to='/about' className='flex flex-col items-center gap-1'>
             <p>About</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
            <NavLink to='/contact' className='flex flex-col items-center gap-1'>
             <p>Contact</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
        
         </ul>
-
+<div className='flex items-center gap-6'>
+  <img src={assets.search_icon} alt="search" className='w-5 cursor-pointer' />
+  
+  <div className='group relative'>
+  <img src={assets.profile_icon} alt="profile" className='w-5 cursor-pointer' />
+  <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4' >
+    <div className='flex flex-col gap-2 w-36 py-3 px-3 bg-slate-100 text-gray-500 rounded'>
+    <p className = 'cursor-pointer hover:text-black'>My Profile</p>
+    <p className = 'cursor-pointer hover:text-black'>Orders</p>
+    <p className = 'cursor-pointer hover:text-black'>Logout</p>
+    </div>
+  </div>
+  </div>
+</div>
     </div>
   )
 }
