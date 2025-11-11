a3213c1472f1ef7ee729dc5da372f907f61fcaec
himanshu-mishu <himanshu.agrl111@gmail.com>
2025-11-11 16:43:23 +0530

    added image

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
 const Hero = () => {
   return (
@@ -18,6 +19,7 @@ const Hero = () => {
         </div>
       </div>
         {/* Hero right side  */}
+        <img className='w-full sm:w-1/2' src={assets.hero_img} alt="hero image" />
     </div>
   )
 }
