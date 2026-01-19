87044eb8ed27f03f4efda82e56a481cc13220569
himanshu-mishu <himanshu.agrl111@gmail.com>
2026-01-20 01:39:42 +0530

    feat: add bestseller section

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const BestSeller = () => {
+  const { products } = useContext(ShopContext);
+  const [bestSeller, setBestSeller] = useState([]);
+
+  useEffect(() => {
+    const bestProduct = products.filter(
+      (product) => product.bestseller === true,
+    );
+
+    setBestSeller(bestProduct.slice(0, 5));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"Best"} text2={"Seller"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-400 pb-8">
+          Discover our bestsellers—customer favorites that combine style,
+          quality, and unbeatable value.
+        </p>
+        {/* Rendering Products */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+          {bestSeller.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
