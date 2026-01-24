41cb4ec6bf8da8037f5d87a33f077ea6eeb4e0b9
himanshu-mishu <himanshu.agrl111@gmail.com>
2026-01-24 13:10:32 +0530

    feat:add news letter box section

---
+++ frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewsLetterBox = () => {
+  return (
+    <div className="text-center">
+      <div className="text-2xl  font-medium text-gray-800">Subscribe now and get 20% off</div>
+    </div>
+  );
+};
+
+export default NewsLetterBox;
