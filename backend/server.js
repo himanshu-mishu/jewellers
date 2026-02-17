b6605ef44c4e5b5503d94a1878c7f0644b8cd285
Himanshu Kumar <himanshukumar@Himanshus-MacBook-Air.local>
2026-02-17 15:27:24 +0530

    feat: setup Express server and MongoDB Atlas connection

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// API Endpoints
+app.get("/", (req, res) => {
+  res.send("API Working!");
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
