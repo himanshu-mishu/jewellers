c66b8ad9de662e0b6c3c6e3e7711aea85882023a
himanshu-mishu <himanshu.agrl111@gmail.com>
2026-02-23 14:59:28 +0530

    feat : add  admin page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Add from "./pages/add.jsx";
+import List from "./pages/List.jsx";
+import Orders from "./pages/Orders.jsx";
+import Login from "./components/Login.jsx";
 
 function App() {
-  return (
-    <>
-  
+  const [token, setToken] = useState("");
 
-      <Navbar />
-    </>
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>{" "}
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
 export default App;
-  
