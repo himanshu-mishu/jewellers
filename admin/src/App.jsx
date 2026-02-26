ee1dff13c19e315c48a23d435acd2be09a22c5dc
himanshu-mishu <himanshu.agrl111@gmail.com>
2026-02-26 19:26:04 +0530

    add list page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -12,6 +12,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = import.meta.env.VITE_CURRENCY;
 
 function App() {
   const [token, setToken] = useState(
