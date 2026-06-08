1363e62fc10d3dc31530a280d1740432ac160e92
himanshu-mishu <himanshu.agrl111@gmail.com>
2026-06-08 16:55:46 +0530

    feat : add order functionality

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,35 +1,35 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
-import Stripe from 'stripe'
-
-
+import Stripe from "stripe";
 
 // place order using Cash on delivery
 const placeOrder = async (req, res) => {
- try {
-        
-        const { userId, items, amount, address} = req.body;
+  try {
+    const { userId, items, amount, address } = req.body;
 
-        const orderData = {
-            userId,
-            items,
-            address,
-            amount,
-            paymentMethod:"COD",
-            payment:false,
-            date: Date.now()
-        }
-        
-        const newOrder = new orderModel(orderData);
-        await newOrder.save();
-        await userModel.findByIdAndUpdate(userId,{cartData:{}})
-        res.status(200).json({message:"Order Placed Successfully"})
-     }
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
 
-        catch (error) {
-          console.log(error);
-        res.status(500).json({message:"Error placing order", error: error.message})
-        }
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(userId, { cartData: {} });
+    res.json({
+      success: true,
+      message: "Order Placed Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Error placing order", error: error.message });
+  }
 };
 
 // place order using stripe
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
-
 // App config
 const app = express();
 const port = process.env.PORT || 4000;
@@ -23,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
-app.use('api/order',orderRouter)
+app.use("/api/order", orderRouter);
 app.get("/", (req, res) => {
   res.send("API Working!");
 });
